a7392a179704ef9da23a7fb1a8c106e03d57c7e2
Maximiliano Gutierrez <maxigutierrez23@gmail.com>
2022-02-28 14:42:07 -0300

    Add pagination to list screencaps endpoint

--- src/handlers/list-screencaps.js
+++ src/handlers/list-screencaps.js
@@ -1,8 +1,9 @@
 const dynamodb = require('aws-sdk/clients/dynamodb');
+const screencapStates = require('/opt/screencap-states.js')
 
 const tableName = process.env.TABLE_NAME;
 
-// https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/DynamoDB/DocumentClient.html#transactWrite-property
+// https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/DynamoDB/DocumentClient.html
 const docClient = new dynamodb.DocumentClient({
     endpoint: process.env.AWS_SAM_LOCAL
         ? 'http://dynamodb:8000'
@@ -10,30 +11,31 @@ const docClient = new dynamodb.DocumentClient({
 });
 
 exports.handler = async (event) => {
-    console.info('received:', event);
+    console.info('received:', event, event.queryStringParameters);
 
     const data = await docClient.query({
         TableName: tableName,
-        KeyConditionExpression: 'UserId = :id',
-        ExpressionAttributeValues: {
-            ":id": event.requestContext.authorizer.claims.sub
-        },
+        KeyConditionExpression: 'UserId = :id and begins_with(#date, :status)',
         ProjectionExpression: '#date, #path, Website',
+        ExpressionAttributeValues: {
+            ":id": event.requestContext.authorizer.claims.sub,
+            ":status": event.queryStringParameters?.status === 'Pending' ? screencapStates.PENDING: screencapStates.COMPLETED
+        },
         ExpressionAttributeNames: {
             "#date": "Date",
             "#path": "Path"
-        }
+        },
+        ScanIndexForward: false,
+        ExclusiveStartKey: event.queryStringParameters?.cursor ? JSON.parse(event.queryStringParameters.cursor) : undefined,
+        Limit: 6
     }).promise();
 
-    console.log('data retrieved', data, data?.Items);
-
-    const screencaps = data.Items;
 
     return {
         statusCode: 200,
         headers: { 
             'Access-Control-Allow-Origin': '*'
         },
-        body: JSON.stringify(screencaps)
+        body: JSON.stringify(data)
     };
 }
